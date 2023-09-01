5f1d022945a88682ef3bdf1b0fb04705d2541160
BogatovVI <vladosdydos@mail.ru>
2023-09-01 15:22:29 +0300

    Added more for test

--- src/test/java/ru/aston/bogatov_vi/task1/ListUsersCreditTest.java
+++ src/test/java/ru/aston/bogatov_vi/task1/ListUsersCreditTest.java
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ListUsersCreditTest {
 
     ListUsersCredit listUsersCredit;
-    TreeSet<Credit> testTree;
+    TreeSet<Integer> testTree;
     Map<Integer, User> testMap;
     List<Credit> ParamList;
+    List<Credit> ParamList2;
 
     @BeforeEach
     void initListUsersCredit() {
@@ -23,14 +23,22 @@ class ListUsersCreditTest {
         User user3 = new User(55, "Нуриев", "Тамерлан");
 
         ParamList = new ArrayList<>();
+        ParamList2 = new ArrayList<>();
         ParamList.add(new CreditAuto(BigDecimal.valueOf(220000), user1, 1));
         ParamList.add(new CreditConsumer(BigDecimal.valueOf(100000), user2, 2));
         ParamList.add(new CreditAuto(BigDecimal.valueOf(50000), user3, 3));
+        ParamList2.add(new CreditAuto(BigDecimal.valueOf(220000), user1, 1));
+        ParamList2.add(new CreditConsumer(BigDecimal.valueOf(1000), user1, 2));
+        ParamList2.add(new CreditAuto(BigDecimal.valueOf(50000), user3, 3));
         listUsersCredit = new ListUsersCredit(ParamList);
 
-        //testTree = new TreeSet<>(ParamList);
-        testMap = new HashMap<>();
+        testTree = new TreeSet<>();
+        testTree.add(20);
+        testTree.add(1);
+        testTree.add(240);
+        testTree.add(6);
 
+        testMap = new HashMap<>();
         testMap.put(1, user1);
         testMap.put(2, user2);
         testMap.put(3, user3);
@@ -57,6 +65,8 @@ class ListUsersCreditTest {
 
     @Test
     void testStruct(){
+        assertFalse(ParamList.equals(ParamList2));
+        assertEquals(testTree.pollLast(), 240);
         ParamList.clear();
     }
 }
